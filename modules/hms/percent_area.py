e78c4c590b6857f53f2ded93fea7419a14e4d2a0
JKob0233 <koblich.joshua@epa.gov>
2018-03-22 14:16:35 -0400

    Added in memory processing, changed object structure and output format

--- modules/hms/percent_area.py
+++ modules/hms/percent_area.py
@@ -1,26 +1,24 @@
 from flask import Response, Flask, request, jsonify
 from flask_restful import Resource, reqparse, abort
 from werkzeug.datastructures import FileStorage
-from shapely.geometry import Point, shape
-import matplotlib.pyplot as plt
-from fiona.crs import from_epsg
 from osgeo import ogr, osr
-import geopandas as geo
 import urllib.request
 from zipfile import *
-import pandas as pd
+import shapefile
+import datetime
 import requests
 import shutil
 import json
 import time
-import uuid
-import sys
-import os
+import io
+import re
 
 parser = reqparse.RequestParser()
 parser.add_argument('huc_8_num')
 parser.add_argument('huc_12_num')
 parser.add_argument('com_id_num')
+parser.add_argument('lat_long_x')
+parser.add_argument('lat_long_y')
 parser.add_argument('filename', location='files', type=FileStorage)
 
 class getPercentArea(Resource):
@@ -30,59 +28,112 @@ class getPercentArea(Resource):
 	http://127.0.0.1:5000/gis/rest/hms/percentage/?huc_8_num=01060002&com_id_num=9311911
 	http://127.0.0.1:5000/gis/rest/hms/percentage/?huc_8_num=01060002
 	http://127.0.0.1:5000/gis/rest/hms/percentage/?huc_12_num=020100050107
+	http://127.0.0.1:5000/gis/rest/hms/percentage/?lat_long_x=-83.5&lat_long_y=33.5
 	'''
 	def get(self):
 		args = parser.parse_args()
 		huc_8_num = args.huc_8_num
 		huc_12_num = args.huc_12_num
 		com_id_num = args.com_id_num
+		latlongx = args.lat_long_x
+		latlongy = args.lat_long_y
 		if(huc_8_num and com_id_num):
-			tab = calculations(huc_8_num, None, com_id_num, None)
+			tab = calculations(huc_8_num, None, com_id_num, None, None)
 		elif(huc_12_num):
-			tab = calculations(None, huc_12_num[0:8], None, None)	#Huc12s are catalogued on ftp server by huc8 numbers
+			tab = calculations(None, huc_12_num[0:8], None, None, None)	#Huc12s are catalogued on ftp server by huc8 numbers
 		elif(huc_8_num):
-			tab = calculations(huc_8_num, None, None, None)
+			tab = calculations(huc_8_num, None, None, None, None)
+		elif(latlongx and latlongy):
+			coord = '(' + latlongx + '+' + latlongy + ')'
+			tab = calculations(None, None, None, None, coord)
 		return jsonify(tab)
 
 	'''
-	User uploads catchment or NHDPlus shapefile
+	User uploads geojson of a catchment or NHDPlus
 	Sample usage:
 	curl -X POST -F 'filename=@file.geojson' http://127.0.0.1:5000/gis/rest/hms/percentage/
 	'''
 	def post(self):
 		args = parser.parse_args()
 		if(args.filename is not None):		#Using curl
-			tab = calculations(None, None, None, args.filename.read())
+			tab = calculations(None, None, None, args.filename.read(), None)
 		else:
 			return Response("{'posting error': 'POST operation failed'}")
 		return jsonify(tab)
 
+class GeometryTable():
+	def __init__(self):
+    		self.geometry = {}   # Dictionary of catchments, where the key is the catchment ID
+    		self.metadata = {}	 # and the value is a Catchment
 
-def calculations(huc8, huc12, com, in_file):
+class Catchment():
+	def __init__(self):
+    		self.points = []     # An array of CatchmentPoint objects
+
+
+class CatchmentPoint():
+	def __init__(self, cellArea, containedArea,	latitude, longitude, percentArea):
+		self.cellArea = cellArea
+		self.containedArea = containedArea
+		self.latitude = latitude
+		self.longitude = longitude
+		self.percentArea = percentArea
+
+def shp_to_geojson(mshp, mdbf):
+	# read the shapefile
+	reader = shapefile.Reader(shp=mshp, dbf=mdbf)
+	fields = reader.fields[1:]
+	field_names = [field[0] for field in fields]
+	buffer = []
+	for sr in reader.shapeRecords():
+		atr = dict(zip(field_names, sr.record))
+		geom = sr.shape.__geo_interface__
+		buffer.append(dict(type="Feature", geometry=geom, properties=atr))
+	# write the GeoJSON file
+	geojson = json.dumps({"type": "FeatureCollection", "features": buffer}, indent=2) + "\n"
+	return geojson
+
+def calculations(huc8, huc12, com, in_file, lat_long):
 	start = time.time()
-	table = []
+	table = GeometryTable()
+	shapeFiles = []
+	nldasFiles = []
 	colNames = []
-	GUID = str(uuid.uuid4())
 	if(in_file):
 		sfile = in_file
+		url = None
 	elif(huc8):
 		url = 'ftp://newftp.epa.gov/exposure/BasinsData/NHDPlus21/NHDPlus' + str(huc8) + '.zip'
-		shapefile = urllib.request.urlretrieve(url, GUID + 'shape.zip')
-		with ZipFile(GUID + 'shape.zip') as myzip:
-			myzip.extractall(GUID)
-		sfile = GUID + '/NHDPlus' + str(huc8) + '/Drainage/Catchment.shp'
+		req = urllib.request.urlopen(url)
+		shzip = ZipFile(io.BytesIO(req.read()))
+		mshp = shzip.open('NHDPlus' + str(huc8) + '/Drainage/Catchment.shp')
+		mdbf = shzip.open('NHDPlus' + str(huc8) + '/Drainage/Catchment.dbf')
+		sfile = shp_to_geojson(mshp, mdbf)
 	elif(huc12):
 		url = 'ftp://newftp.epa.gov/exposure/NHDV1/HUC12_Boundries/' + str(huc12) + '.zip'
-		shapefile = urllib.request.urlretrieve(url, GUID + 'shape.zip')
-		with ZipFile(GUID + 'shape.zip') as myzip:
-			myzip.extractall(GUID)
-		sfile = GUID + '/' + str(huc12) + '/huc12.shp'
+		req = urllib.request.urlopen(url)
+		shzip = ZipFile(io.BytesIO(req.read()))
+		mshp = shzip.open(str(huc12) + '/huc12.shp')
+		mdbf = shzip.open(str(huc12) + '/huc12.dbf')
+		sfile = shp_to_geojson(mshp, mdbf)
+	elif(lat_long):
+		url = 'https://ofmpub.epa.gov/waters10/SpatialAssignment.Service?pGeometry=POINT' + lat_long + '&pLayer=NHDPLUS_CATCHMENT&pSpatialSnap=TRUE&pReturnGeometry=TRUE'
+		req = urllib.request.urlopen(url)
+		sfile = GUID + 'shape.geojson'
+		string = open(sfile, 'r')
+		re_str = string.read()
+		geojson = re.search(r'(?={"type").*(]})', re_str)
+		out_file = open(sfile, 'w')
+		out_file.write(geojson.group(0))
+		out_file.close()
+		string.close()
 	nldasurl = 'https://ldas.gsfc.nasa.gov/nldas/gis/NLDAS_Grid_Reference.zip'
-	gridfile = urllib.request.urlretrieve(nldasurl, GUID + 'grid.zip')
-	with ZipFile(GUID + 'grid.zip') as myzip:
-		myzip.extractall(GUID + 'NLDAS')
-	gfile = GUID + 'NLDAS/NLDAS_Grid_Reference.shp'
-	print("FINISHED UNZIPPING: ", time.time() - start)
+	resp = urllib.request.urlopen(nldasurl)
+	gridzip = ZipFile(io.BytesIO(resp.read()))
+	gshp = gridzip.open('NLDAS_Grid_Reference.shp')
+	gdbf = gridzip.open('NLDAS_Grid_Reference.dbf')
+	gfile = shp_to_geojson(gshp, gdbf)
+	print("FINISHED CONVERTING: ", time.time() - start)
 	shape = ogr.Open(sfile)
 	nldas = ogr.Open(gfile)
 	shapeLayer = shape.GetLayer()
@@ -116,6 +167,12 @@ def calculations(huc8, huc12, com, in_file):
 			polygons.append(feature)
 			huc8s.append(feature.GetField('HUC_8'))
 			huc12s.append(feature.GetField('HUC_12'))
+	else:
+		coms = [None] * len(shapeLayer)
+		huc8s = [None] * len(shapeLayer)
+		huc12s = [None] * len(shapeLayer)
+		for feature in shapeLayer:
+			polygons.append(feature)
 	# Reproject geometries from shapefile
 	totalPoly = ogr.Geometry(ogr.wkbMultiPolygon)
 	#Treat all polygons as one larger one since we are just finding overlapping cells
@@ -123,7 +180,10 @@ def calculations(huc8, huc12, com, in_file):
 		poly = polygon.GetGeometryRef()
 		poly.Transform(coordTrans)
 		totalPoly.AddGeometry(poly)
-	totalPoly = totalPoly.UnionCascaded()
+	if(lat_long):
+		totalPoly = polygons[0].GetGeometryRef()
+	else:
+		totalPoly = totalPoly.UnionCascaded()
 	# Calculate cells that contain polygons ahead of time to make intersections faster
 	# This block of code should be parallelized if possible
 	for feature in nldasLayer:
@@ -131,13 +191,15 @@ def calculations(huc8, huc12, com, in_file):
 		if (totalPoly.Intersects(cell) and feature not in overlap):
 			overlap.append(feature)
 	# Iterate through smaller list of overlapping cells to calculate data
-	huc8table = [{"HUC 8 ID": huc8s[0]}]
+	#table.geometry = {"HUC 8 ID": huc8s[0]}
+	#huc8table = [{"HUC 8 ID": huc8s[0]}]
 	i = 0;
+	num_points = 0
 	for polygon in polygons:
 		poly = polygon.GetGeometryRef()
-		huc12table = []
-		huc12table.append({"HUC 12 ID": huc12s[i]})
-		huc12table.append({"Catchment ID": coms[i]})
+		huc12table = Catchment()
+		#huc12table.points.append({"HUC 12 ID": huc12s[i]})
+		#huc12table.points.append({"Catchment ID": coms[i]})
 		for feature in overlap:
 			cell = feature.GetGeometryRef()
 			interArea = 0
@@ -146,29 +208,19 @@ def calculations(huc8, huc12, com, in_file):
 				inter = poly.Intersection(cell)
 				interArea += inter.Area()
 				percentArea = (interArea / squareArea) * 100
-				catchtable = {"latitude": cell.Centroid().GetX(),
-							  "longitude": cell.Centroid().GetY(),
-							  "cellArea": squareArea,
-							  "containedArea": interArea,
-							  "percentArea": percentArea}
-				huc12table.append(catchtable)
-		huc8table.append(huc12table)
+				catchtable = CatchmentPoint(squareArea, interArea, cell.Centroid().GetX(), cell.Centroid().GetY(), percentArea)
+				huc12table.points.append(catchtable.__dict__)
+				num_points += 1
+		table.geometry[coms[i]] = huc12table.__dict__
+		#huc8table.append(huc12table)
 		i += 1
-	table.append(huc8table)
-	# Delete zipfiles and extracted shapefiles
+	#table.geometry.append(huc8table)
+	table.metadata['request date'] = datetime.datetime.now()
+	table.metadata['number of points'] = num_points
+	table.metadata['shapefile source'] = url
+	table.metadata['nldas source'] = nldasurl
+	table.metadata['execution time'] = time.time() - start
+	# De-reference shapefiles
 	shape = None
 	nldas = None
-	if(in_file):
-		os.remove(GUID + 'grid.zip')
-		shutil.rmtree(GUID + 'NLDAS')
-	else:
-		os.remove(GUID + 'grid.zip')
-		os.remove(GUID + 'shape.zip')
-		shutil.rmtree(GUID + 'NLDAS')
-		shutil.rmtree(GUID)
-	#jsonOut = json.dumps(table)
-	# write json to file?
-	#print(jsonOut)
-	end = time.time()
-	print(end - start)
-	return table
+	return table.__dict__
